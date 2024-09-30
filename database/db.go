0d031f9bb424dccccd0dea6353fab5a3f9f9a51e
张声录 <zhangshenglu@idszt.com>
2024-09-30 11:16:33 +0800

    给GORMDB增加日志相关的配置

--- database/db.go
+++ database/db.go
@@ -4,6 +4,10 @@ import (
 	"fmt"
 	"gorm.io/driver/mysql"
 	"gorm.io/gorm"
+	"gorm.io/gorm/logger"
+	"log"
+	"os"
+	"time"
 )
 
 var GLOBALDB *gorm.DB
@@ -12,7 +16,15 @@ func init() {
 	var err error
 	// 参考 https://github.com/go-sql-driver/mysql#dsn-data-source-name 获取详情
 	dsn := "root:my-secret-pw@tcp(192.168.188.101:3306)/test?charset=utf8mb4&parseTime=True&loc=Local"
-	GLOBALDB, err = gorm.Open(mysql.Open(dsn), &gorm.Config{})
+	GLOBALDB, err = gorm.Open(mysql.Open(dsn), &gorm.Config{
+		Logger: logger.New(log.New(os.Stdout, "\r\n", log.LstdFlags), logger.Config{
+			SlowThreshold:             time.Second, // Slow SQL threshold
+			LogLevel:                  logger.Info, // Log level
+			IgnoreRecordNotFoundError: true,        // Ignore ErrRecordNotFound error for logger
+			ParameterizedQueries:      false,       // true: Don't include params in the SQL log
+			Colorful:                  false,       // Disable color
+		}),
+	})
 	if err != nil {
 		panic(err)
 	}
