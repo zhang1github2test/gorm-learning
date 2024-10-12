9fcf20a79ad85c7f413854c4a2cf654efb0d9468
张声录 <zhangshenglu@idszt.com>
2024-10-12 18:08:30 +0800

    GORM自定义插件实现

--- callback/decrypt.go
+++ callback/decrypt.go
@@ -2,6 +2,7 @@ package callback
 
 import (
 	"gorm.io/gorm"
+	"reflect"
 )
 
 func Register(db *gorm.DB) {
@@ -20,11 +21,32 @@ type DecryptCreate interface {
 func Decrypt(db *gorm.DB) {
 	if db.Error == nil && db.Statement.Schema != nil && !db.Statement.SkipHooks {
 		callMethod(db, func(value interface{}, tx *gorm.DB) (called bool) {
-			if i, ok := value.(DecryptCreate); ok {
-				called = true
-				db.AddError(i.Decrypt(tx))
+			reflectValue := reflect.ValueOf(value)
+			typeofRe := reflect.TypeOf(value)
+			if typeofRe.Kind() != reflect.Ptr {
+				called = false
+				return called
 			}
+			reflectValue = reflectValue.Elem()
+			typeofRe = typeofRe.Elem()
 
+			// Iterate over the fields
+			for i := 0; i < typeofRe.NumField(); i++ {
+				field := typeofRe.Field(i)
+
+				// Get the "encryption" tag
+				encryptionTag := field.Tag.Get("encryption")
+
+				// Check if the encryption tag is set to "true"
+				if encryptionTag == "true" {
+					ecryptStr := reflectValue.Field(i).String()
+					decrypt, err := AesDecrypt(ecryptStr, DATA_KEY)
+					if err != nil {
+						db.AddError(err)
+					}
+					reflectValue.Field(i).SetString(decrypt)
+				}
+			}
 			return called
 		})
 	}
